bc42720535750e83fb7d06dded6a57881dcad875
Anthony Balashov <anthony.balashov@gmail.com>
2021-03-09 13:01:30 +0300

    HW1

---
+++ src/lesson1/Orange.java
@@ -0,0 +1,17 @@
+package lesson1;
+
+/**
+ * Апельсинки
+ */
+public class Orange extends Fruit {
+
+    Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString(){
+        return "апельсины";
+    }
+
+}
